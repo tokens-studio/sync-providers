98691e1f1040cff580dcc8cbb32f56d9bf6907a1
SorsOps <80043879+sorsOps@users.noreply.github.com>
2025-04-15 13:43:25 +0200

    Remove GET_TOKEN_SET_PAGE from GET_PROJECT_DATA_QUERY

--- packages/fetch-from-ts/src/queries/GET_PROJECT_DATA_QUERY.ts
+++ packages/fetch-from-ts/src/queries/GET_PROJECT_DATA_QUERY.ts
@@ -19,26 +19,3 @@ export const GET_PROJECT_DATA_QUERY = gql`
   }
 `;
 
-export const GET_TOKEN_SET_PAGE = gql`
-  query TokenSetsPage(
-    $projectId: String!
-    $organization: String!
-    $branch: String!
-    $page: Int!
-  ) {
-    project(id: $projectId, organization: $organization) {
-      branch(name: $branch) {
-        tokenSets(page: $page, limit: 100) {
-          nextPage
-          totalPages
-          data {
-            name
-            orderIndex
-            type
-            raw
-          }
-        }
-      }
-    }
-  }
-`;
